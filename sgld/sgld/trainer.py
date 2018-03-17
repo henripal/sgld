ccd750fb08eb635a19de63590bf6b5b16f23853a
henripal <henri.palacci@gmail.com>
2018-03-17 16:16:47 -0400

    slightly modified end_experment

--- sgld/sgld/trainer.py
+++ sgld/sgld/trainer.py
@@ -11,13 +11,13 @@ import xpbase
 
 def runall(cuda_device,
     model_desc):
-    
-    
-    
-    
+
+
+
+
     db_string = "postgres://postgres:1418@localhost/experiments"
     xpbase.initialize(db_string)
-    
+
     torch.cuda.set_device(cuda_device)
     model = MnistModel()
     train_loader, test_loader = sgld.make_datasets()
@@ -32,7 +32,7 @@ def runall(cuda_device,
         cuda_device
     )
 
-    return xp 
+    return xp
 
 def sample(model_desc, model_params, percentage_tosample):
     """
@@ -44,15 +44,15 @@ def sample(model_desc, model_params, percentage_tosample):
         result.append(rng.choice(np.ravel(v),
                                  size=int(len(np.ravel(v))*percentage_tosample),
                                 replace=False))
-        
+
     result = np.hstack(result)
-    
+
     return result
-        
-    
+
+
 
 def train(model, train_loader, test_loader, optimizer, model_desc, cuda_device):
-    i = 0 
+    i = 0
     lossfunc = lambda x: sgld.lossrate(x,
                                        model_desc['a'],
                                        model_desc['b'],
@@ -68,7 +68,7 @@ def train(model, train_loader, test_loader, optimizer, model_desc, cuda_device):
     std_max = 0
 
     for epoch in range(model_desc['epochs']):
-        
+
         model.train()
         for data, target in train_loader:
             i += 1
@@ -82,25 +82,25 @@ def train(model, train_loader, test_loader, optimizer, model_desc, cuda_device):
             optimizer.step(lossfunc(i), add_noise=model_desc['addnoise'])   # update gradients
             prediction = output.data.max(1)[1]   # first column has actual prob.
             accuracy = np.mean(prediction.eq(target.data))*100
-            
+
             statedict = model.state_dict().copy()
             for k, v in statedict.items():
                 statedict.update({k: v.cpu().numpy().tolist()})
             #parameter_history.append(statedict)
-            
-            
-            stdev_acc.append(sample(model_desc, 
+
+
+            stdev_acc.append(sample(model_desc,
                                     statedict,
                                     model_desc['percentage_tosample']))
-            
+
             if i%model_desc['step_samples'] == 0:
                 sample_mat = np.vstack(stdev_acc)
                 stdev_acc = []
                 stdevs = np.std(sample_mat, axis = 0)
                 std_median = np.median(stdevs)
                 std_max = np.max(stdevs)
-                
-                
+
+
             if i%100 == 0:
                 model.eval()
                 for data, target in test_loader:
@@ -110,30 +110,32 @@ def train(model, train_loader, test_loader, optimizer, model_desc, cuda_device):
                     output = model(data)
                     prediction = output.data.max(1)[1]   # first column has actual prob.
                     val_accuracy = np.mean(prediction.eq(target.data))*100
-                    
+
                 statedict = model.state_dict().copy()
                 for k, v in statedict.items():
                     statedict.update({k: v.cpu().numpy().tolist()})
             else:
                 state_dict = {}
-                    
+
             xpbase.step_experiment(xp, i,
                                    trainloss=loss.data[0],
                                    trainacc=accuracy,
                                    valacc=val_accuracy,
                                    epoch=epoch,
-                                   model_params={'statedict': statedict, 
-                                                 'std_median': std_median,
-                                                 'std_max': std_max}
+                                   custom_fields={'std_median': std_median,
+                                                 'std_max': std_max},
+                                   model_params=state_dict
                                   )
-            
+
         print('Epoch: {}\tLoss: {:.3f}\tAcc: {:.3f}\tVal Acc: {:.3f}'.format(epoch,
                                                                 np.mean(loss.data[0]),
                                                                 np.mean(accuracy),
                                                                 np.mean(val_accuracy)))
-        
+
     print('Total number of steps: {}'.format(i))
-    
-    xpbase.end_experiment(xp)
+
+    xpbase.end_experiment(xp, final_trainloss=np.mean(loss.data[0]),
+                          final_trainacc=np.mean(accuracy),
+                          final_valacc=np.mean(val_accuracy))
 
     return xp
